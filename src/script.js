83f2db44f9a863ab4959c4f0cf0ec732d8280f33
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-03-15 15:37:24 +0100

    script.js under progress

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
 function playeronescorecalc() {
-  let totalscore = 31;
+  let totalscore = 301;
   let currentscore = 0;
   let latestscore = totalscore - currentscore;
   currentscore = latestscore;
   console.log(currentscore);
   let currentscoreElement = document.getElementById("ply1crntscr");
+  console.log(currentscoreElement.value);
+  let player1 = document.getElementById("player1").value;
+  console.log(player1.value);
   currentscoreElement.innerHTML = currentscore;
 }
 
 let plybtn1 = document.getElementById("buttonply1");
 let plybtn2 = document.getElementById("buttonply1");
 plybtn1.addEventListener("click", playeronescorecalc());
+
 //plybtn2.addEventListener("click", playertwoscorecalc());

cfcb38197c5c7a41ae7fac9dd471a6e5f49b956f
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-03-16 14:51:32 +0100

    modified 2playerhtml

--- src/script.js
+++ src/script.js
@@ -11,8 +11,16 @@ function playeronescorecalc() {
   currentscoreElement.innerHTML = currentscore;
 }
 
+function player1scoreupdater() {
+  let score1 = document.getElementById("score1");
+  console.log(score1.value);
+}
 let plybtn1 = document.getElementById("buttonply1");
 let plybtn2 = document.getElementById("buttonply1");
-plybtn1.addEventListener("click", playeronescorecalc());
+//plybtn1.addEventListener("click", playeronescorecalc());
+let score1 = document.getElementById("score1");
+console.log(score1);
+let submit1 = document.getElementById("submit1");
+submit1.addEventListener("click", player1scoreupdater());
 
 //plybtn2.addEventListener("click", playertwoscorecalc());
